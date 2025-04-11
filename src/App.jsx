c432207b1c174744257b13637eb1b53ff5a09ce4
jeet <jeetbhavsar0510@gmail.com>
2025-04-11 20:04:38 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
     };
 
     try {
-      const res = await fetch('http://localhost:5000/send-email', {
+      const res = await fetch("/api/contact", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
